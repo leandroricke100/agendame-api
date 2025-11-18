f145f6dac74538c79911f0b2b63b7d0d823b5f01
Leandro Henrique Silva Lima <leandrohenriquesilvalima@gmail.com>
2025-11-17 18:19:49 -0700

    rota de registro

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginControler;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\User\MeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', LoginControler::class)->name('login'); // __invoke()
+Route::post('/login', LoginControler::class)->name('login');
+
+Route::post('register', RegisterController::class);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('me', [MeController::class, 'show']);
