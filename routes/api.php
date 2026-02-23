fa8305dc59785b7026312266a21009be44435bf1
Leandro Henrique Silva Lima <leandrohenriquesilvalima@gmail.com>
2026-02-23 09:47:08 -0300

    envio de EMAIL

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Auth\LoginControler;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\User\MeController;
+use App\Mail\WelcomeMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', LoginControler::class)->name('login');
@@ -12,4 +14,9 @@ Route::post('register', RegisterController::class);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('me', [MeController::class, 'show']);
+});
+
+
+Route::post('email', function () {
+    Mail::to('teste@teste.com')->send(new WelcomeMail());
 });
